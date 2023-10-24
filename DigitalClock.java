bec11883a88c282ff4df8101139651247eecee76
Lochan78 <148063231+Lochan78@users.noreply.github.com>
2023-10-24 15:18:54 +0530

    Add files via upload

---
+++ DigitalClock.java
@@ -0,0 +1,25 @@
+// import java.text.SimpleDateFormat;
+// import java.util.Date;
+
+// public class DigitalClock {
+//     public static void main(String[] args) {
+//         while (true) {
+//             // Get the current time
+//             Date currentTime = new Date();
+            
+//             // Format the time to display in HH:mm:ss format
+//             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+//             String formattedTime = sdf.format(currentTime);
+            
+//             // Print the formatted time
+//             System.out.print("\r" + formattedTime);
+            
+//             try {
+//                 // Wait for 1 second before updating the time
+//                 Thread.sleep(1000);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
